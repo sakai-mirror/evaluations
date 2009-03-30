2b6b1fb41bb9777c365bbed49a1001ab8edbb0da
lovemore.nalube@uct.ac.za <lovemore.nalube@uct.ac.za@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-03-30 09:59:43 +0000

    Fixed itemTemplates not showing settings controls due to cache issue.
    git-svn-id: https://source.sakaiproject.org/contrib/uct/evaluation-mar09@59418 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/facebox/facebox.js
+++ tool/src/webapp/content/js/facebox/facebox.js
@@ -416,6 +416,7 @@
         $('#facebox table').attr('width', 700);
         $('#facebox .body').css('width',660);
         $('#facebox .header').eq(0).show();
+		$.facebox.defaults.objToUpdate = null;
       })
         $(document).trigger('afterClose.facebox');
         return false
