ce2695f10c9fbb181be05ad12568670083c3d7e5
stephen.marquard@uct.ac.za <stephen.marquard@uct.ac.za@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-04-03 12:08:10 +0000

    EVALSYS-647 EVALSYS-695 Merge r59397:59398 from trunk
    git-svn-id: https://source.sakaiproject.org/contrib/uct/evaluation-mar09@59583 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- impl/src/java/org/sakaiproject/evaluation/logic/EvalAuthoringServiceImpl.java
+++ impl/src/java/org/sakaiproject/evaluation/logic/EvalAuthoringServiceImpl.java
@@ -527,6 +527,13 @@ public class EvalAuthoringServiceImpl implements EvalAuthoringService {
     public EvalTemplateItem getTemplateItemById(Long templateItemId) {
         log.debug("templateItemId:" + templateItemId);
         EvalTemplateItem templateItem = (EvalTemplateItem) dao.findById(EvalTemplateItem.class, templateItemId);
+        if (templateItem != null) {
+            if (TemplateItemUtils.isBlockParent(templateItem)) {
+                // get the children
+                List<EvalTemplateItem> children = getBlockChildTemplateItemsForBlockParent(templateItem.getId(), false);
+                templateItem.childTemplateItems = children;
+            }
+        }
         return templateItem;
     }
 
